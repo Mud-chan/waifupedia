d19a30e4945bd417cb8f1439ac18affbe293004a
pramudya <e41220957@student.polije.ac.id>
2023-08-28 22:02:18 +0700

    waifupedia

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="hehe.css">
+</head>
+<body>
+    
+<section>
+
+<div class="backgrond">   
+    
+    <video autoplay loop muted  src="../gambar/raidenvideo.mp4"></video>
+    <div class="atas">
+        <div class="icon"><img src="../gambar/waifupedia.png" alt=""></div>
+        <div class="menu"><a href="">HOME</a> <a href="">CHARAKTER</a> <a href="">KELOLA WAIFU</a></div>
+        <div class="logi"><a href="">LOG OUT</a></div>    
+    </div>
+    <div class="selamatdatang">
+        <p class="a">Telusuri Informasi Tentang Waifumu Disini</p> 
+        <img src="../gambar/raiden.png" alt="">
+        <img src="../gambar/hutao.png" alt="">   
+    </div>
+    
+    
+    
+    <img id="bg" src="../gambar/latarbelakang1.png" alt="">
+    <img id="bg" src="../gambar/latarbelakang1.png" alt="">
+    <img id="bg" src="../gambar/sakurabawah.png" alt="">
+    
+</div>
+<div class="pembatas"></div>
+<div class="bawah">
+    <img style="width: 190px; height: auto; top:0px;" src="../gambar/kucing.png" alt="">
+    <p style="font-size: 23px; color: white;top:0px;">Created By</p>
+    <p style="font-size: 23px; color: white;top:0px;">Lewd Chan</p>
+</div>
+    
+</section>
+<a href="#" class="ignielToTop"></a>
+</body>
+</html>
